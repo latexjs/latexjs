1db584d48c313d1b6dd401a6d2ed78d02e7a3a58
James Booth <jabooth@gmail.com>
2017-04-13 09:30:26 +0100

    use incoming emscripten, compile to WASM

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd emscripten && docker build . -t latexjs/emscripten && cd ..
 cd texlive && docker build . -t latexjs/texlive && cd ..
 cd thintexlive && docker build . -t latexjs/thintexlive && cd ..
-cd latexjs && docker build . -t latexjs/latexjs && cd ..
+cd latexjs && docker build . -t latexjs/latexjs:wasm && cd ..
 cd server && docker build . -t latexjs/server && cd ..
--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 docker push latexjs/server
-docker push latexjs/latexjs
+docker push latexjs/latexjs:wasm
 docker push latexjs/thintexlive
 docker push latexjs/texlive
 docker push latexjs/emscripten
